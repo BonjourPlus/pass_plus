7a7c15ed9c7fb67f85e73752342d658b3f51d972
Jasdero <houssemaine.j@gmail.com>
2017-02-24 09:43:01 +0100

    securing the ajax request for statuses

--- src/Jasdero/PassePlatBundle/Controller/StateController.php
+++ src/Jasdero/PassePlatBundle/Controller/StateController.php
@@ -16,6 +16,35 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class StateController extends Controller
 {
+
+
+    //token management
+    protected function csrfProtect()
+    {
+        // store token into session
+        $session = $this->get('session');          // expired token treatment
+        $expire = $session->get('bjp_token_expire');
+        if (!empty($expire) && ($expire < time())) {
+            $session->remove('bjp_token');
+            $session->remove('bjp_token_expire');
+        }          // token generation
+        if (empty($session->get('bjp_token'))) {
+            $session->set('bjp_token', bin2hex(random_bytes(10)));
+            $session->set('bjp_token_expire', time() + 3600);
+        }
+        return $session->get('bjp_token');
+    }
+
+    //override render method to send token
+    public function render($view, array $parameters = array(), Response $response = null)
+    {
+        // generate token
+        $parameters['bjptoken'] = $this->csrfProtect();
+        // call to parent render()
+        return parent::render($view, $parameters, $response);
+    }
+
+    //also sending token to compare when doing Ajax
     /**
      * Lists all state entities.
      *
@@ -25,11 +54,11 @@ class StateController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-
-        $states = $em->getRepository('JasderoPassePlatBundle:State')->findBy([],['weight'=>'DESC']);
+        $states = $em->getRepository('JasderoPassePlatBundle:State')->findBy([], ['weight' => 'DESC']);
 
         return $this->render('state/index.html.twig', array(
             'states' => $states,
+            'token' => $this->get('session')->get('bjp_token'),
         ));
     }
 
@@ -114,13 +143,13 @@ class StateController extends Controller
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->remove($state);
-            $em->flush($state);
+            $em->flush();
         }
 
         return $this->redirectToRoute('state_index');
     }
 
-    //attempt to change statuses weights from Ajax call
+    //method to change statuses weights from Ajax call
     /**
      *
      * @Route("/dynamicChange", name="weight_change")
@@ -130,25 +159,58 @@ class StateController extends Controller
     public function weightChange(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
+        $responseText = '';
+
+        dump($request);
+        //checking Ajax
+        if (!$request->isXmlHttpRequest()) {
+            $responseText = 'ERROR';
+        }
+
+        //checking bjp-token
+        if ($request->request->get('token') !== $request->getSession()->get('bjp_token')) {
+            $responseText = 'ERROR';
+        }
 
         //retrieving new table order
         $newOrder = $request->request->get('request');
-        array_shift($newOrder);
 
-        //setting statuses weights
-        foreach ($newOrder as $key=>$stateId) {
-            $state = $em->getRepository('JasderoPassePlatBundle:State')->findOneBy(['id'=>$stateId]);
-            $state->setWeight(1000-($key*100));
-            $em->persist($state);
-            $em->flush();
+        //checking array type
+        if (!is_array($newOrder)) {
+            $responseText = 'ERROR';
         }
 
-        //updating orders statuses
-        $orders = $em->getRepository('JasderoPassePlatBundle:Orders')->findAll();
-        foreach ($orders as $order) {
-            $this->get('orderstatus')->orderStatusAction($order);
+        //checking array values
+        foreach ($newOrder as $value) {
+            if (preg_match('#\D#', $value)) {
+                $responseText = 'ERROR';
+            }
         }
-        return new Response();
+
+        //redirect to home if something wrong
+        if ($responseText == 'ERROR') {
+            return $this->redirectToRoute('home');
+        } else {
+            //doing the changes if everything is OK
+
+            //cleaning array
+            array_shift($newOrder);
+
+            //setting statuses weights
+            foreach ($newOrder as $key => $stateId) {
+                $state = $em->getRepository('JasderoPassePlatBundle:State')->findOneBy(['id' => $stateId]);
+                $state->setWeight(1000 - ($key * 100));
+                $em->persist($state);
+                $em->flush();
+            }
+
+            //updating orders statuses
+            $orders = $em->getRepository('JasderoPassePlatBundle:Orders')->findAll();
+            foreach ($orders as $order) {
+                $this->get('orderstatus')->orderStatusAction($order);
+            }
+            return new Response();
+        }
     }
 
     /**
@@ -163,7 +225,6 @@ class StateController extends Controller
         return $this->createFormBuilder()
             ->setAction($this->generateUrl('state_delete', array('id' => $state->getId())))
             ->setMethod('DELETE')
-            ->getForm()
-        ;
+            ->getForm();
     }
 }
