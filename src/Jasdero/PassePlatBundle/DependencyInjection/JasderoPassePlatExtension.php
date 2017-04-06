fcf2f1188abfd1b0fcbaacedac93f413cac97c1e
Jasdero <houssemaine.j@gmail.com>
2017-04-06 14:58:02 +0200

    drive made optional

--- src/Jasdero/PassePlatBundle/DependencyInjection/JasderoPassePlatExtension.php
+++ src/Jasdero/PassePlatBundle/DependencyInjection/JasderoPassePlatExtension.php
@@ -26,6 +26,9 @@ class JasderoPassePlatExtension extends Extension
         $container->setParameter( 'folder_to_scan', $processedConfig['folders']['to_scan'] );
         $container->setParameter( 'new_orders_folder', $processedConfig['folders']['new_orders'] );
         $container->setParameter( 'errors_folder', $processedConfig['folders']['errors'] );
+        $container->setParameter( 'drive_activation', $processedConfig['activation'] );
+        $container->setParameter('path_to_refresh_token', $processedConfig['credentials']['path_to_refresh_token']);
+        $container->setParameter('auth_config', $processedConfig['credentials']['auth_config']);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
