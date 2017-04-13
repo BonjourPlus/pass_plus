bb165d00d0d7eec02d724aa1c5adaa57320f976e
Jasdero <houssemaine.j@gmail.com>
2017-04-13 14:41:47 +0200

    categories creation

---
+++ src/Jasdero/PassePlatBundle/Form/Type/CategoryType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Jasdero\PassePlatBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Jasdero\PassePlatBundle\Entity\Category'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'jasdero_passeplatbundle_category';
+    }
+
+
+}
