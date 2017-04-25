1877d505fad291cd34acdf6761571e605ff88f1b
Jasdero <houssemaine.j@gmail.com>
2017-04-25 16:33:03 +0200

    first scenarios

--- src/Jasdero/PassePlatBundle/Tests/Controller/MainControllerTest.php
+++ src/Jasdero/PassePlatBundle/Tests/Controller/MainControllerTest.php
@@ -6,12 +6,41 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class MainControllerTest extends WebTestCase
 {
-    public function testIndex()
+
+    public function testDashboard()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/dashboard');
 
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertCount(1, $crawler->filter('nav'));
+    }
+
+    /**
+     * @dataProvider linksProvider
+     * @param $linkContent
+     */
+    public function testLinksNavSettings($linkContent)
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/dashboard');
+
+        $link = $crawler->selectLink($linkContent)->link();
+
+        $targetPage = $client->click($link);
+
+        $this->assertEquals($linkContent.' list', $targetPage->filter('h5')->first()->text());
+        $this->assertCount(1, $targetPage->filter('table'));
+    }
+
+    public function linksProvider()
+    {
+        return array(
+          array('Catalogs'),
+          array('Categories'),
+          array('Statuses'),
+          array('Vats'),
+        );
     }
 }
